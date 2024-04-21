5d79c851becb001cdd8a95e40dae34be28e97a73
Ramin <aliyevramin2019@gmail.com>
2024-04-21 02:36:28 -0700

    Modify FilterChain

--- src/main/java/com/example/loginregistration/config/SecurityConfiguration.java
+++ src/main/java/com/example/loginregistration/config/SecurityConfiguration.java
@@ -4,39 +4,48 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@RequiredArgsConstructor
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf()
-                .disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/v1/auth/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        http.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/v1/auth/register").permitAll();
+                    auth.requestMatchers("/v1/auth/login").permitAll();
+                    auth.requestMatchers(permitSwagger).permitAll();
+                    auth.anyRequest().authenticated();
+                });
 
+        http.authenticationProvider(authenticationProvider);
+        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
+        
         return http.build();
     }
 
+    public static String[] permitSwagger = {
+            "/api/v1/auth/",
+            "v3/api-docs/",
+            "v3/api-docs.yanl",
+            "swagger-ui/",
+            "swagger-ui.html"
+    };
 }
--- src/main/java/com/example/loginregistration/service1/JwtService.java
+++ src/main/java/com/example/loginregistration/service1/JwtService.java
@@ -16,7 +16,7 @@ import java.util.function.Function;
 
 @Service
 public class JwtService {
-    private static final String SECRET_KEY = "4E645267556B58703272357538782F413F4428472B4B6250655368566D597133";
+    private static final String SECRET_KEY = "jia9oOr4X1k5xi45FC6cDdHk/gnjsO1q+DMz5nvyQgU=";
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
